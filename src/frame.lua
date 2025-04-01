90775c353e7f2b6d0c1c2d83392399c64061d4ce
Pizzahawaiii <ksorrr@gmail.com>
2025-04-01 20:24:25 +0200

    share: decouple sharing logic from frame

--- src/frame.lua
+++ src/frame.lua
@@ -19,56 +19,12 @@ local function initFrame(f, anchor)
     if IsShiftKeyDown() then
       if arg1 ~= 'LeftButton' and arg1 ~= 'RightButton' and arg1 ~= 'MiddleButton' and arg1 ~= 'Button4' then return end
 
-      local text
       if f.timer then
-        local city = f.timer.faction == 'A' and T['SW'] or T['OG']
-        local timer = PWB_timers[f.timer.faction][f.timer.boss]
-        local suffix = T['head has no timer, it\'s probably despawned']
-
-        if timer then
-          local h, m = PWB.core.getTimeLeft(timer.h, timer.m)
-          local isNow = h == 0 and m == 0
-          suffix = isNow and T['head will despawn NOW!'] or T['head will despawn in'] .. ' ' .. PWB.utils.toString(h, m)
-        end
-
-        text = '(' .. city .. ') ' .. T[PWB.Bosses[f.timer.boss]] .. ' ' .. suffix
-      elseif f.name == 'PizzaWorldBuffsDmf' then
-        text = 'Darkmoon Faire location is currently unknown. Try "/w Tents dmf?"'
-
-        if PWB.utils.hasDmf() then
-          local location = PWB.DmfLocations[PWB_dmf.location]
-          local secondsAgo = time() - PWB_dmf.seenAt
-          local timeAgo = PWB.utils.toRoughTimeString(secondsAgo)
-          text = 'Darkmoon Faire was last seen in ' .. location .. ' ' .. timeAgo .. ' ago.'
-        end
+        PWB.share.share(arg1, 'timer', f.timer)
+      elseif f.name == 'PizzaWorldBuffsDmf' then 
+        PWB.share.share(arg1, 'dmf')
       else
-        local prefix = T['I\'m using PizzaWorldBuffs. Get it at']
-        if math.random(1, 20) == 1 then
-          prefix = T['I <3 pineapple on pizza! Get some at']
-        end
-        text = prefix .. ' https://github.com/Pizzahawaiii/PizzaWorldBuffs'
-      end
-
-      if text then
-        local editBox = DEFAULT_CHAT_FRAME.editBox
-        local targetEditBox = WIM_EditBoxInFocus or editBox:IsVisible() and editBox or nil
-        if targetEditBox then
-          targetEditBox:SetText(text)
-        elseif arg1 == 'Button4' then
-          local worldChannelId = PWB.utils.getChannelId('world')
-          if worldChannelId then
-            SendChatMessage(text, 'CHANNEL', nil, worldChannelId)
-          end
-        else
-          local sendTo
-          if arg1 == 'LeftButton' then sendTo = 'SAY' end
-          if arg1 == 'RightButton' then sendTo = 'GUILD' end
-          if arg1 == 'MiddleButton' then sendTo = 'HARDCORE' end
-
-          if sendTo then
-            SendChatMessage(text, sendTo)
-          end
-        end
+        PWB.share.share(arg1, 'link')
       end
     else
       if not PWB_config.lock then
---
+++ src/share.lua
@@ -0,0 +1,61 @@
+local PWB = PizzaWorldBuffs
+PWB.share = {}
+
+setfenv(1, PWB:GetEnv())
+
+function PWB.share.share(mouseButton, type, t)
+  if type == 'timer' and not t then return end
+  if mouseButton ~= 'LeftButton' and mouseButton ~= 'RightButton' and mouseButton ~= 'MiddleButton' and mouseButton ~= 'Button4' then return end
+
+  local text
+  if type == 'timer' then
+    local city = t.faction == 'A' and T['SW'] or T['OG']
+    local timer = PWB_timers[t.faction][t.boss]
+    local suffix = T['head has no timer, it\'s probably despawned']
+
+    if timer then
+      local h, m = PWB.core.getTimeLeft(timer.h, timer.m)
+      local isNow = h == 0 and m == 0
+      suffix = isNow and T['head will despawn NOW!'] or T['head will despawn in'] .. ' ' .. PWB.utils.toString(h, m)
+    end
+
+    text = '(' .. city .. ') ' .. T[PWB.Bosses[timer.boss]] .. ' ' .. suffix
+  elseif type == 'dmf' then
+    text = 'Darkmoon Faire location is currently unknown. Try "/w Tents dmf?"'
+
+    if PWB.utils.hasDmf() then
+      local location = PWB.DmfLocations[PWB_dmf.location]
+      local secondsAgo = time() - PWB_dmf.seenAt
+      local timeAgo = PWB.utils.toRoughTimeString(secondsAgo)
+      text = 'Darkmoon Faire was last seen in ' .. location .. ' ' .. timeAgo .. ' ago.'
+    end
+  elseif type == 'link' then
+    local prefix = T['I\'m using PizzaWorldBuffs. Get it at']
+    if math.random(1, 20) == 1 then
+      prefix = T['I <3 pineapple on pizza! Get some at']
+    end
+    text = prefix .. ' https://github.com/Pizzahawaiii/PizzaWorldBuffs'
+  end
+
+  if text then
+    local editBox = DEFAULT_CHAT_FRAME.editBox
+    local targetEditBox = WIM_EditBoxInFocus or editBox:IsVisible() and editBox or nil
+    if targetEditBox then
+      targetEditBox:SetText(text)
+    elseif arg1 == 'Button4' then
+      local worldChannelId = PWB.utils.getChannelId('world')
+      if worldChannelId then
+        SendChatMessage(text, 'CHANNEL', nil, worldChannelId)
+      end
+    else
+      local sendTo
+      if arg1 == 'LeftButton' then sendTo = 'SAY' end
+      if arg1 == 'RightButton' then sendTo = 'GUILD' end
+      if arg1 == 'MiddleButton' then sendTo = 'HARDCORE' end
+
+      if sendTo then
+        SendChatMessage(text, sendTo)
+      end
+    end
+  end
+end
