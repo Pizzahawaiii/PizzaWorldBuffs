5e6ea4f2a0d957aef0b68710f9f14c417f805465
blu5hit <magdatiberiuandrei@gmail.com>
2025-04-10 19:05:51 +0300

    Update

--- src/frame.lua
+++ src/frame.lua
@@ -1,3 +1,4 @@
+-- frame.lua
 local PWB = PizzaWorldBuffs
 
 setfenv(1, PWB:GetEnv())
@@ -18,7 +19,6 @@ local function initFrame(f, anchor)
   f.frame:SetScript('OnMouseDown', function ()
     if IsShiftKeyDown() then
       if arg1 ~= 'LeftButton' and arg1 ~= 'RightButton' and arg1 ~= 'MiddleButton' and arg1 ~= 'Button4' then return end
-
       if f.timer then
         PWB.share.share(arg1, 'timer', f.timer)
       elseif f.name == 'PizzaWorldBuffsDmf' then 
@@ -40,18 +40,15 @@ local function initFrame(f, anchor)
     f.frame:SetScript('OnEnter', function ()
       local location
       local lastSeen
-
       if PWB_dmf and PWB.DmfLocations[PWB_dmf.location] then
         local locationColor = PWB_dmf.location == 'E' and PWB.Colors.alliance or PWB.Colors.horde
         location = locationColor .. PWB.DmfLocations[PWB_dmf.location]
-
         local lastSeenSeconds = time() - PWB_dmf.seenAt
         local lastSeenColor = PWB.Colors.green
         if lastSeenSeconds > (60 * 60) then lastSeenColor = PWB.Colors.orange end
         if lastSeenSeconds > (60 * 60 * 12) then lastSeenColor = PWB.Colors.red end
         lastSeen = lastSeenColor .. PWB.utils.toRoughTimeString(lastSeenSeconds)
       end
-
       GameTooltip_SetDefaultAnchor(GameTooltip, UIParent)
       GameTooltip:SetText(PWB.Colors.primary .. 'Darkmoon Faire')
       if location and lastSeen then
@@ -87,7 +84,7 @@ local function initFrames()
     {
       name = 'PizzaWorldBuffsHeader',
       text = PWB.Colors.primary .. 'Pizza' .. PWB.Colors.secondary .. 'WorldBuffs',
-      shouldShow =  function() 
+      shouldShow = function() 
         return PWB_config.header == true
       end,
     },
@@ -121,7 +118,7 @@ local function initFrames()
     },
     {
       name = 'PizzaWorldBuffsDmf',
-      shouldShow =  function() 
+      shouldShow = function() 
         return PWB_config.dmf == true
       end,
     },
@@ -142,7 +139,6 @@ PWB.frame:RegisterEvent('PLAYER_ENTERING_WORLD')
 PWB.frame:SetScript('OnEvent', function ()
   if event == 'PLAYER_ENTERING_WORLD' then
     initFrames()
-
     if PWB_config.show then
       PWB.frame:Show()
     else
@@ -152,18 +148,26 @@ PWB.frame:SetScript('OnEvent', function ()
 end)
 
 function PWB.frame.updateFrames()
+  local spacing = 10 -- Space between frames in horizontal mode
+  local xOffset = 0
+  local yOffset = 0
+  local maxWidth = 0
+  local totalHeight = 0
+
+  -- Debug: Confirm orientation is being read
+  -- PWB:Print("Current orientation: " .. PWB_config.orientation)
+
   for i, frame in ipairs(PWB.frames) do
     frame.frame.text:SetFont(STANDARD_TEXT_FONT, PWB_config.fontSize, 'OUTLINE')
 
+    -- Update text content
     if frame.timer then
       local timer = PWB_timers and PWB_timers[frame.timer.faction] and PWB_timers[frame.timer.faction][frame.timer.boss]
       local timeStr = PWB.Colors.grey .. T['N/A']
-
       if timer then
         local h, m = PWB.core.getTimeLeft(timer.h, timer.m)
         timeStr = PWB.utils.getTimerColor(timer.witness, timer.receivedFrom) .. PWB.utils.toString(h, m)
       end
-
       local bossColor = frame.timer.faction == 'A' and PWB.Colors.alliance or PWB.Colors.horde
       frame.frame.text:SetText(bossColor .. T[PWB.Bosses[frame.timer.boss]] .. ': ' .. timeStr)
 
@@ -174,7 +178,6 @@ function PWB.frame.updateFrames()
       end
     elseif frame.name == 'PizzaWorldBuffsDmf' then
       local location = PWB.Colors.grey .. 'N/A'
-
       if PWB_dmf then
         local shortLoc = PWB.utils.strSplit(PWB.DmfLocations[PWB_dmf.location], ' ')
         if shortLoc then
@@ -182,37 +185,53 @@ function PWB.frame.updateFrames()
           location = color .. shortLoc
         end
       end
-
       frame.frame.text:SetText(PWB.Colors.primary .. 'DMF: ' .. location)
     end
 
-    frame.frame:ClearAllPoints()
-    local y = frame.anchor and (-frame.anchor.frame.text:GetHeight()) or 0
-    local point = PWB_config.align == 'left' and 'TOPLEFT' or PWB_config.align == 'right' and 'TOPRIGHT' or 'TOP'
-
-    local idx = i - 1
-    -- Anchor DMF frame correctly if only own faction timers are shown.
-    if frame.name == 'PizzaWorldBuffsDmf' and not PWB_config.allFactions then
-      idx = i - 3
-    end
-    frame.frame:SetPoint(point, 0, idx * y)
-    frame.frame:SetWidth(frame.frame.text:GetWidth())
-    frame.frame:SetHeight(frame.frame.text:GetHeight())
-
+    -- Handle visibility for optional frames
     if frame.shouldShow then
       if frame.shouldShow() then
         frame.frame:Show()
       else
-        frame.frame:SetHeight(0)
         frame.frame:Hide()
       end
     end
+
+    -- Position frames based on orientation
+    frame.frame:ClearAllPoints()
+    local point = PWB_config.align == 'left' and 'TOPLEFT' or PWB_config.align == 'right' and 'TOPRIGHT' or 'TOP'
+
+    if PWB_config.orientation == 'horizontal' then
+      if frame.frame:IsShown() then
+        frame.frame:SetPoint(point, PWB.frame, point, xOffset, 0)
+        xOffset = xOffset + frame.frame.text:GetWidth() + spacing
+        totalHeight = math.max(totalHeight, frame.frame.text:GetHeight())
+      end
+    else -- vertical (default)
+      if frame.frame:IsShown() then
+        frame.frame:SetPoint(point, PWB.frame, point, 0, yOffset)
+        yOffset = yOffset - frame.frame.text:GetHeight()
+        maxWidth = math.max(maxWidth, frame.frame.text:GetWidth())
+        totalHeight = totalHeight + frame.frame.text:GetHeight()
+      end
+    end
+
+    frame.frame:SetWidth(frame.frame.text:GetWidth())
+    frame.frame:SetHeight(frame.frame.text:GetHeight())
+  end
+
+  -- Set parent frame size
+  if PWB_config.orientation == 'horizontal' then
+    PWB.frame:SetWidth(xOffset - spacing) -- Subtract last spacing
+    PWB.frame:SetHeight(totalHeight)
+  else
+    PWB.frame:SetWidth(maxWidth)
+    PWB.frame:SetHeight(totalHeight)
   end
 end
 
 -- Update
 PWB.frame:SetScript('OnUpdate', function ()
-  -- Throttle this function so it doesn't run on every frame render
   if (this.tick or 1) > GetTime() then return else this.tick = GetTime() + 1 end
   PWB.frame.updateFrames()
 end)
